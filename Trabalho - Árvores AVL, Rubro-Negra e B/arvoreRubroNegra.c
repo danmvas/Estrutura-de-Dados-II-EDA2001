665cd2bb95a51d74d313713e9d8083d627f1e3dd
arturvma <57048958+arturvma@users.noreply.github.com>
2021-08-13 23:19:52 -0300

    Update arvoreRubroNegra.c
    alterado MAIN para ficar compatível com a árvore. Vetor espelhando INORDER para inserção em ordem crescente e contadores de interações

--- Trabalho - Árvores AVL, Rubro-Negra e B/arvoreRubroNegra.c
+++ Trabalho - Árvores AVL, Rubro-Negra e B/arvoreRubroNegra.c
@@ -1,23 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int interacoes = 0;
+
+// Structure to represent each
+// node in a red-black tree
 struct node {
-	int d;
-	int c;
-	struct node* p; 
-	struct node* r; 
-	struct node* l; 
+	int d; // data
+	int c; // 1-red, 0-black
+	struct node* p; // parent
+	struct node* r; // right-child
+	struct node* l; // left child
 };
 
+// global root for the entire tree
 struct node* root = NULL;
+int *vetor; //cria uma cópia ordenada da árvore para inserir como pior caso
+int posicoes = 0;
 
-
-struct node* bst(struct node* trav,
-					struct node* temp)
+// function to perform BST insertion of a node
+struct node* bst(struct node* trav, struct node* temp)
 {
+	// If the tree is empty,
+	// return a new node
 	if (trav == NULL)
 		return temp;
 
+	// Otherwise recur down the tree
 	if (temp->d < trav->d)
 	{
 		trav->l = bst(trav->l, temp);
@@ -28,10 +37,14 @@ struct node* bst(struct node* trav,
 		trav->r = bst(trav->r, temp);
 		trav->r->p = trav;
 	}
+    interacoes++;
 
+	// Return the (unchanged) node pointer
 	return trav;
 }
 
+// Function performing right rotation
+// of the passed node
 void rightrotate(struct node* temp)
 {
 	struct node* left = temp->l;
@@ -47,8 +60,11 @@ void rightrotate(struct node* temp)
 		temp->p->r = left;
 	left->r = temp;
 	temp->p = left;
+    interacoes++;
 }
 
+// Function performing left rotation
+// of the passed node
 void leftrotate(struct node* temp)
 {
 	struct node* right = temp->r;
@@ -64,8 +80,11 @@ void leftrotate(struct node* temp)
 		temp->p->r = right;
 	right->l = temp;
 	temp->p = right;
+    interacoes++;
 }
 
+// This function fixes violations
+// caused by BST insertion
 void fixup(struct node* root, struct node* pt)
 {
 	struct node* parent_pt = NULL;
@@ -74,14 +93,22 @@ void fixup(struct node* root, struct node* pt)
 	while ((pt != root) && (pt->c != 0)
 		&& (pt->p->c == 1))
 	{
-		parent_pt = pt->p;
+		interacoes++;
+        parent_pt = pt->p;
 		grand_parent_pt = pt->p->p;
 
+		/* Case : A
+			Parent of pt is left child
+			of Grand-parent of
+		pt */
 		if (parent_pt == grand_parent_pt->l)
 		{
 
 			struct node* uncle_pt = grand_parent_pt->r;
 
+			/* Case : 1
+				The uncle of pt is also red
+				Only Recoloring required */
 			if (uncle_pt != NULL && uncle_pt->c == 1)
 			{
 				grand_parent_pt->c = 1;
@@ -92,12 +119,18 @@ void fixup(struct node* root, struct node* pt)
 
 			else {
 
+				/* Case : 2
+					pt is right child of its parent
+					Left-rotation required */
 				if (pt == parent_pt->r) {
 					leftrotate(parent_pt);
 					pt = parent_pt;
 					parent_pt = pt->p;
 				}
 
+				/* Case : 3
+					pt is left child of its parent
+					Right-rotation required */
 				rightrotate(grand_parent_pt);
 				int t = parent_pt->c;
 				parent_pt->c = grand_parent_pt->c;
@@ -106,9 +139,16 @@ void fixup(struct node* root, struct node* pt)
 			}
 		}
 
+		/* Case : B
+			Parent of pt is right
+			child of Grand-parent of
+		pt */
 		else {
 			struct node* uncle_pt = grand_parent_pt->l;
 
+			/* Case : 1
+				The uncle of pt is also red
+				Only Recoloring required */
 			if ((uncle_pt != NULL) && (uncle_pt->c == 1))
 			{
 				grand_parent_pt->c = 1;
@@ -117,13 +157,18 @@ void fixup(struct node* root, struct node* pt)
 				pt = grand_parent_pt;
 			}
 			else {
-
+				/* Case : 2
+				pt is left child of its parent
+				Right-rotation required */
 				if (pt == parent_pt->l) {
 					rightrotate(parent_pt);
 					pt = parent_pt;
 					parent_pt = pt->p;
 				}
 
+				/* Case : 3
+					pt is right child of its parent
+					Left-rotation required */
 				leftrotate(grand_parent_pt);
 				int t = parent_pt->c;
 				parent_pt->c = grand_parent_pt->c;
@@ -136,84 +181,86 @@ void fixup(struct node* root, struct node* pt)
 	root->c = 0;
 }
 
+// Function to print inorder traversal
+// of the fixated tree
 void inorder(struct node* trav)
 {
 	if (trav == NULL)
 		return;
 	inorder(trav->l);
-	printf("%d ", trav->d);
+	//printf("%d ", trav->d);
+    vetor[posicoes++] = trav->d;
 	inorder(trav->r);
 }
 
-///////////////////////////////////////////////////////////////////////////////////////
-
+// driver code
 int main(){
-    int item, ch;
-    int tamAmostra = 63;
-    int values[tamAmostra];
+    int item; //recebe o valor aleatório para adicionar à árvor
+    int tamAmostra = 63; //quantidade de nodes a serem adicionadas
+    int *values; // armazena os nodes
+    values = malloc(tamAmostra*sizeof(struct node));
+    vetor = malloc(tamAmostra*sizeof(int));
 
-    printf("---------------------------------------------\n");
-    printf("\t\tÁrvore Rubro Negra\n\n");
-    printf("---------------------------------------------\n");
-    printf("\t\tCaso medio\n\n");
-    printf("Tamanho dos Conjuntos | Esforco Computacional\n");
-    for (int j = 0; j < 10; j++){
+    
+    //serão geradas 10 árvores
+    for (int j = 0; j < 2; j++){
+        printf("---------------------------------------------\n");
+        printf("\t\tCaso medio\n\n");
+        printf("Tamanho dos Conjuntos | Esforco Computacional\n");
 
+        //determina a quantidade de nodes
         int tamAmostra = rand() % 100;
-        int values[tamAmostra];
+        values = realloc(values, tamAmostra*sizeof(struct node));
+        vetor = realloc(vetor, tamAmostra*sizeof(int));
 
+        //para cada árvore, adiciona seus nodes com valores aleatórios
         for(int i=0; i < tamAmostra; i++){
-            values[i] = rand() % 1000;
-        }
+            item = rand() % 1000;
 
-		// alterar pra essa árvore!
-        for(int i = 0; i < tamAmostra; i++){
-            insercao(values[i]);
+            //prepara o node para ser inserido
+            struct node* temp = (struct node*)malloc(sizeof(struct node));
+            temp->r = NULL;
+            temp->l = NULL;
+            temp->p = NULL;
+            temp->d = item; //recebe o valor aleatório
+            temp->c = 1; //cor vermelha
+
+            //insere o node
+            root = bst(root, temp);
         }
         
         printf("\t   %d         | \t%d\n", tamAmostra, interacoes);
         interacoes = 0;
-    }
-
-    printf("---------------------------------------------\n");
-    printf("\t\tPior Caso\n\n");
-    printf("Tamanho dos Conjuntos | Esforco Computacional\n");
-    for (int j = 0; j < 10; j++){
-
-        int tamAmostra = rand() % 100;
-        int values[tamAmostra];
-
+        inorder(root);
+        
+    
+        printf("---------------------------------------------\n");
+        printf("\t\tPior Caso (decrescente)\n\n");
+        printf("Tamanho dos Conjuntos | Esforco Computacional\n");
+    
+        //para cada árvore, adiciona os mesmos nodes em ordem crescente
         for(int i=0; i < tamAmostra; i++){
-            values[i] = rand() % 1000;
-        }
+            item = vetor[i];
+            //printf("%i ", item);
 
-        for(int i = 0; i < tamAmostra; i++){
-            insercao(values[i]);
+            //prepara o node para ser inserido
+            struct node* temp = (struct node*)malloc(sizeof(struct node));
+            temp->r = NULL;
+            temp->l = NULL;
+            temp->p = NULL;
+            temp->d = item; //recebe o valor aleatório
+            temp->c = 1; //cor vermelha
+
+            //insere o node
+            root = bst(root, temp);
         }
         
-        printf("\t   %d         | \t%d\n", tamAmostra, interacoes);
+        printf("\n\t   %d         | \t%d\n", tamAmostra, interacoes);
         interacoes = 0;
+        posicoes = 0;
+        root = NULL;
     }
+    //inorder(root);
 
-
-	// int n = 7;
-	// int a[7] = { 7, 6, 5, 4, 3, 2, 1 };
-
-	// for (int i = 0; i < n; i++) {
-
-	// 	struct node* temp = (struct node*)malloc(sizeof(struct node));
-	// 	temp->r = NULL;
-	// 	temp->l = NULL;
-	// 	temp->p = NULL;
-	// 	temp->d = a[i];
-	// 	temp->c = 1;
-
-	// 	root = bst(root, temp);
-
-	// 	fixup(root, temp);
-	// }
-
-	// printf("Inoder Traversal of Created Tree\n");
-	// inorder(root);
-    // printf("\n");
+    return 0;
 }
